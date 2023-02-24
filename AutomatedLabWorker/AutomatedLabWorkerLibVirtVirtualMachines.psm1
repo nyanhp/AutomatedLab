9f73a27e71d4d31067ca657eca970917f3ae831e
Jan-Hendrik Peters [MSFT] <nyanhp@users.noreply.github.com>
2023-02-24 09:27:35 +0100

    Implement usage of OS Architecture (#1495)
    * Implement usage of OS Architecture
    
    * Update changelog and help

---
+++ AutomatedLabWorker/AutomatedLabWorkerLibVirtVirtualMachines.psm1
@@ -0,0 +1,88 @@
+﻿function Get-LWLibVirtVm
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [AutomatedLab.Machine]
+        $Machine
+    )
+}
+
+function New-LWLibVirtVm
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [AutomatedLab.Machine]
+        $Machine
+    )
+}
+
+function Remove-LWLibVirtVm
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [AutomatedLab.Machine]
+        $Machine
+    )
+}
+
+function Start-LWLibVirtVm
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [AutomatedLab.Machine]
+        $Machine
+    )
+}
+
+function Stop-LWLibVirtVm
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [AutomatedLab.Machine]
+        $Machine
+    )
+}
+
+function Save-LWLibVirtVm
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [AutomatedLab.Machine]
+        $Machine
+    )
+}
+
+function Checkpoint-LWLibVirtVm
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [AutomatedLab.Machine]
+        $Machine
+    )
+}
+
+function Restore-LWLibVirtVm
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [AutomatedLab.Machine]
+        $Machine
+    )
+}
+
